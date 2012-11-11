a979047f9ef91c7c689231414e3e2baa6406cc44
Michael Anzuoni <me.anzuoni@gmail.com>
2012-11-11 14:10:51 -0500

    Skeleton code

---
+++ src/robotController.java
@@ -0,0 +1,70 @@
+import roombacomm.*;
+
+public class robotController {
+	
+	int robotMood = 0;
+	
+	// positive mood thresholds
+	int h1 = 4;
+	int h2 = 8;
+	int h3 = 12;
+	int h4 = 16;
+	int h5 = 20;
+	
+	// negative mood threshold
+	int s1 = -4;
+	int s2 = -8;
+	int s3 = -12;
+	int s4 = -16;
+	int s5 = -20;
+	
+	public static void h1MoodAction(){
+		
+	}
+	
+	public static void h2MoodAction(){
+		
+	}
+	
+	public static void h3MoodAction(){
+		
+	}
+	
+	public static void h4MoodAction(){
+		
+	}
+	
+	public static void h5MoodAction(){
+		
+	}
+	
+	public static void s1MoodAction(){
+		
+	}
+	
+	public static void s2MoodAction(){
+		
+	}
+	
+	public static void s3MoodAction(){
+		
+	}
+	
+	public static void s4MoodAction(){
+		
+	}
+	
+	public static void s5MoodAction(){
+		
+	}
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
