d123182f48bb3bb47aeb3b8a22b4e0a23d783fd4
Michael Anzuoni <me.anzuoni@gmail.com>
2012-11-16 17:30:22 -0500

    Fixed up shannon's code

--- src/robotController.java
+++ src/robotController.java
@@ -1,70 +1,120 @@
 import roombacomm.*;
+import java.lang.*;
+import java.util.Random;
 
 public class robotController {
-	
-	int robotMood = 0;
-	
-	// positive mood thresholds
-	int h1 = 4;
-	int h2 = 8;
-	int h3 = 12;
-	int h4 = 16;
-	int h5 = 20;
-	
-	// negative mood threshold
-	int s1 = -4;
-	int s2 = -8;
-	int s3 = -12;
-	int s4 = -16;
-	int s5 = -20;
-	
-	public static void h1MoodAction(){
-		
-	}
-	
-	public static void h2MoodAction(){
-		
-	}
-	
-	public static void h3MoodAction(){
-		
-	}
-	
-	public static void h4MoodAction(){
-		
-	}
-	
-	public static void h5MoodAction(){
-		
-	}
-	
-	public static void s1MoodAction(){
-		
-	}
-	
-	public static void s2MoodAction(){
-		
-	}
-	
-	public static void s3MoodAction(){
-		
-	}
-	
-	public static void s4MoodAction(){
-		
-	}
-	
-	public static void s5MoodAction(){
-		
-	}
-	
+  
+  static //Tribble tribble = new Tribble();
+  RoombaCommSerial roombacomm = new RoombaCommSerial();
+  static String roombacommPort = "/dev/ttyS0";
+  Random random = new Random();
+ 
+ int robotMood = 0;
+ 
+ // positive mood thresholds
+ int h1 = 4;
+ int h2 = 8;
+ int h3 = 12;
+ int h4 = 16;
+ int h5 = 20;
+ 
+ // negative mood threshold
+ int s1 = -4;
+ int s2 = -8;
+ int s3 = -12;
+ int s4 = -16;
+ int s5 = -20;
+ 
+ public void h1MoodAction(){
+   //Happy
+   
+   roombacomm.drive(5,5); //fast-ish
+   // roombacomm.Drive serialportname 100 -1 waittime
+   
+   if (random.nextFloat() > .5) {
+  // tribble.purr();
+   }
+ }
+ 
+ public void h2MoodAction(){
+   //Elated
+   
+   //roombacomm.waggle();
+   
+   if (random.nextFloat() > .5) {
+   roombacomm.playNote(91, 50);
+   roombacomm.playNote(96, 150);
+   }
+   
+   if (random.nextFloat() > .5) {
+  // tribble.createTribblePurrSong();
+   }
+ }
+ 
+ public void h3MoodAction(){
+  
+ }
+ 
+ public void h4MoodAction(){
+  
+ }
+ 
+ public void h5MoodAction(){
+  
+ }
+ 
+ public void s1MoodAction(){
+  
+ }
+ 
+ public void s2MoodAction(){
+  
+ }
+ 
+ public void s3MoodAction(){
+   //Depressed
+   
+    if (random.nextFloat() > .4) {
+      roombacomm.drive(2,2); //slowly
+   }
+  
+    if (random.nextFloat() > .3) {
+   roombacomm.playNote(45, 100);
+   roombacomm.playNote(43, 100);
+   roombacomm.playNote(42, 100);
+   roombacomm.playNote(41, 100);
+   }
+  
+ }
+ 
+ public void s4MoodAction(){
+   //The robot hates its life...
+   
+   if (random.nextFloat() > .1) {
+     roombacomm.playNote(38, 200);
+     roombacomm.pause(100);
+     roombacomm.playNote(36, 200);
+   }
+   
+   if (random.nextFloat() > .1) {
+     roombacomm.drive(1,1); //very small increments, slowly
+   }
+ }
+ 
+ public void s5MoodAction(){
+   
+ 
+ }
+ 
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+ /**
+  * @param args
+  */
+ public static void main(String[] args) {
+	 
+	 roombacomm.connect(roombacommPort);
 
 
-	}
+ }
 
 }
